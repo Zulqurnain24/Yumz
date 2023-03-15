6f610d6427568d9dfaa3756dbe35c8e8818b7af1
Muhammad Zulqurnain <muhammad@kalido.me>
2023-03-15 23:32:26 +0000

    Initial Commit

---
+++ Yumz/YumzApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumzApp.swift
+//  Yumz
+//
+//  Created by Muhammad Zulqurnain on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct YumzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
